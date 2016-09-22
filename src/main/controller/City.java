eddf7e155f888b16583461d0ebf9830bef003724
Mathieu <mpeyramard@yahoo.fr>
2016-09-22 10:44:15 +0200

    développement méthode askDataFromMySQLBDD + add fichier

---
+++ src/main/controller/City.java
@@ -0,0 +1,23 @@
+package main.controller;
+
+import java.util.ArrayList;
+
+/**
+ * This is the class who's got the specification of a city:
+ * his name, longitude and latitude. Also a specific id.
+ * @author Mathieu
+ */
+public class City {
+
+	private int id;
+	private String name;
+	private float lon;
+	private float lat;
+	private String country;
+	
+	
+	public void city() {
+		
+	}
+
+}
