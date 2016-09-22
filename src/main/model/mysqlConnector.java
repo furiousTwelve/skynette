e0ad4926ed0f2151bd21254d2471ab13f34c9799
david di Marcantonio <david@afpa>
2016-09-22 15:16:37 +0200

    add file to dialogue with databases

---
+++ src/main/model/mysqlConnector.java
@@ -0,0 +1,17 @@
+package main.model;
+
+/**
+ * Implements the connector to dialogue with the external MYSQL database
+ * @author david
+ *
+ */
+
+public class mysqlConnector 
+{
+
+	public mysqlConnector() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/model/sqliteConnector.java
@@ -0,0 +1,15 @@
+package main.model;
+
+/**
+ * Implements the connector to dialogue with the internal app SQLITE database
+ * @author david
+ *
+ */
+public class sqliteConnector 
+{
+
+	public sqliteConnector() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
