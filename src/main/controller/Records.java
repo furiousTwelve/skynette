ae1dd468bad640177dca5ef0e4d696dee0727774
Mathieu <mpeyramard@yahoo.fr>
2016-09-22 15:15:52 +0200

    modif dans records et dataTransfer

--- src/main/controller/Records.java
+++ src/main/controller/Records.java
@@ -23,5 +23,110 @@ public class Records {
 		
 	}
 	
+	//========================
+	//GETTERS AND SETTERS
+	//========================
+	
+
+
+	public float getTemp() {
+		return temp;
+	}
+
+
+	public void setTemp(float temp) {
+		this.temp = temp;
+	}
+
+
+	public float getPressure() {
+		return pressure;
+	}
+
+
+	public void setPressure(float pressure) {
+		this.pressure = pressure;
+	}
+
+
+	public int getHumidity() {
+		return humidity;
+	}
+
+
+	public void setHumidity(int humidity) {
+		this.humidity = humidity;
+	}
+
+
+	public float getSpeed() {
+		return speed;
+	}
+
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+
+
+	public float getDeg() {
+		return deg;
+	}
+
+
+	public void setDeg(float deg) {
+		this.deg = deg;
+	}
+
+
+	public float getRain() {
+		return rain;
+	}
+
+
+	public void setRain(float rain) {
+		this.rain = rain;
+	}
+
+
+	public int getClouds() {
+		return clouds;
+	}
+
+
+	public void setClouds(int clouds) {
+		this.clouds = clouds;
+	}
+
+
+	public int getSnow() {
+		return snow;
+	}
+
+
+	public void setSnow(int snow) {
+		this.snow = snow;
+	}
+
+
+	public String getWeatherDescription() {
+		return weatherDescription;
+	}
+
+
+	public void setWeatherDescription(String weatherDescription) {
+		this.weatherDescription = weatherDescription;
+	}
+
+
+	public String getWindDirection() {
+		return windDirection;
+	}
+
+
+	public void setWindDirection(String windDirection) {
+		this.windDirection = windDirection;
+	}
+
 	
 }
