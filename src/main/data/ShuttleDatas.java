8014c2457dc777086ffec8f7ae91ec6e747eb1f9
34011-82-01 <34011-82-01@exchange.ad.afpanet>
2016-09-22 14:27:43 +0200

    Commit with a new class in main data

---
+++ src/main/data/ShuttleDatas.java
@@ -0,0 +1,18 @@
+package main.data;
+
+/**
+ * @author AnaisGueyte
+ * @date 22/09/2016
+ * @version Skynette 0.1
+ * 
+ *          This claims aims to pass fresh datas from SQLite database to the
+ *          controller methods updateIcon() & updateMainWindow() and update the
+ *          datas in the widget and main Window
+ *
+ */
+
+public class ShuttleDatas {
+	
+	
+
+}
