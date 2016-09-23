0ddd93dec509ed3f6735c0fc51fe7fdb4f46f943
khadidja <34011-82-04@exchange.ad.afpanet>
2016-09-23 08:25:33 +0200

    version avant derniere

--- src/main/presentation/PanelMeteo.java
+++ src/main/presentation/PanelMeteo.java
@@ -7,26 +7,27 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class PanelMeteo extends JPanel{
-	
+public class PanelMeteo extends JPanel {
+
 	protected JPanel panelDay = new JPanel();
-	protected JLabel sunnyDaysIcone = new JLabel();
-	ImageIcon iconWind = new ImageIcon(new ImageIcon("..\\..\\git\\skynette\\icon_weather\\wind.png").getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT));
+	protected JLabel sunnyDaysIcon = new JLabel();
+	ImageIcon iconWind = new ImageIcon(new ImageIcon("..\\..\\git\\skynette\\icon_weather\\wind.png").getImage()
+			.getScaledInstance(70, 70, Image.SCALE_DEFAULT));
 	protected JLabel temperatureIcone1 = new JLabel();
 	protected JLabel temperature = new JLabel("27°");
 	protected JLabel windDirection = new JLabel(" NNO ");
 	protected JLabel windSpeed1 = new JLabel("13km/h");
 	protected JLabel windIcone = new JLabel(iconWind);
+	protected JLabel date = new JLabel(" ");
 	GridLayout g2 = new GridLayout(6, 1);
-	
-	
-	public PanelMeteo(){
+
+	public PanelMeteo() {
 		panelDay.setLayout(g2);
-		
+		panelDay.add(date);
+		panelDay.add(sunnyDaysIcon);
+		panelDay.add(temperature);
+		panelDay.add(windIcone);
+		panelDay.add(windSpeed1);
 	}
-	
-	
-	
-	
 
 }
